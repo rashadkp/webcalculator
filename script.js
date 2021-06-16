d728d5c29f1d21aa7c12e8edc9e0f3675614ee98
rashadkp <63586227+rashadkp@users.noreply.github.com>
2021-06-16 23:08:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function btnclick(val){
+    document.getElementById("screen").value+=val
+}
+function clearscrn(){
+    document.getElementById("screen").value=""
+}
+function equalClick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
